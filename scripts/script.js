6d831f74d405dcce754aed6bf17c3fb4baf3920f
Raghunath javare <raghunath.javare@gmail.com>
2023-03-07 18:11:30 +0530

    adding inline javascript in html

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Number Elemets
+
 const headerText = document.querySelector(".header-text");
 const oneNumber = document.querySelector(".one");
 const twoNumber = document.querySelector(".two");
@@ -9,60 +11,16 @@ const sevenNumber = document.querySelector(".seven");
 const eightNumber = document.querySelector(".eight");
 const nineNumber = document.querySelector(".nine");
 const zeroNumber = document.querySelector(".zero");
-console.log(oneNumber,twoNumber,threeNumber,fourNumber,fiveNumber,sixNumber,sevenNumber,eightNumber,nineNumber,zeroNumber);
 
+// Operator Elements
+const opeClear = document.querySelector(".zero");
+const opeBackSpace = document.querySelector(".zero");
+const opeDivider = document.querySelector(".zero");
+const opeMinus = document.querySelector(".zero");
+const opeMultiply = document.querySelector(".zero");
+const opeAddition = document.querySelector(".zero");
+const opeEqual = document.querySelector(".zero");
 
 const addNumber = function (val) {
   document.querySelector(".header-text").value += val;
 };
-
-
-oneNumber.addEventListener("click", function () {
-  addNumber("1");
-});
-
-
-twoNumber.addEventListener("click", function () {
-  addNumber("2");
-});
-
-
-threeNumber.addEventListener("click", function () {
-  addNumber("3");
-});
-
-
-fourNumber.addEventListener("click", function () {
-  addNumber("4");
-});
-
-
-fiveNumber.addEventListener("click", function () {
-  addNumber("5");
-});
-
-
-
-sixNumber.addEventListener("click", function () {
-  addNumber("6");
-});
-
-
-sevenNumber.addEventListener("click", function () {
-  addNumber("7");
-});
-
-
-eightNumber.addEventListener("click", function () {
-  addNumber("8");
-});
-
-
-nineNumber.addEventListener("click", function () {
-  addNumber("9");
-});
-
-zeroNumber.addEventListener("click", function () {
-  addNumber("0");
-});
-
