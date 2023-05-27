8b5cddd36bd0cb97748eaf8499adaf121a83ea75
Raghunath javare <raghunath.javare@gmail.com>
2023-05-27 09:44:40 +0530

    inserting zero in user input

--- scripts/script.js
+++ scripts/script.js
@@ -3,28 +3,44 @@ const numberContainer = document.querySelector(".number__container");
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-let value;
+let value = "";
+userInput.value = 0;
+
+// inserting zero
+const zero = function (value) {
+  userInput.value = value;
+};
+
+// Inserting a calculation
 
 numberContainer.addEventListener("click", function (e) {
   e.preventDefault();
   const event = e.target;
 
-  // if value is not a number
-  if (event.classList.contains("buttons"))
+  if (event.classList.contains("buttons")) {
+    if (!event) return;
+    zero(value);
     // display numbers
     value = userInput.value += event.value;
+  }
 
-  // operation with
+  // operation with user value
 
   if (event.classList.contains("btn__backspace")) {
     if (!value) return;
-    // removing last charecter
+
+    // removing last charecters
+
     value = value.slice(0, -1);
     userInput.value = value;
+    if (value.length < 1) userInput.value = 0;
   }
   //  removeing all numbers
 
-  if (event.classList.contains("del__num")) userInput.value = "";
+  if (event.classList.contains("del__num")) {
+    zero(0);
+    value = "";
+  }
 
   if (event.classList.contains("btn__equal") && value)
     value = userInput.value = `${eval(value)}`;
