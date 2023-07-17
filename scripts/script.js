1d3a169e7ba071b0d7dd82301ac0e0fbd1cf8dcd
Raghunath javare <raghunath.javare@gmail.com>
2023-07-17 11:20:06 +0530

    converting my app to opp

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,73 @@
 const userInput = document.querySelector(".user__input");
 const numberContainer = document.querySelector(".number__container");
-const delNum = document.querySelector(".del__num");
+const btnDelAll = document.querySelector(".btn-delete__all");
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
+class App {
+  #value = "";
 
-let value = "";
-userInput.value = 0;
-
-// inserting value
-
-
-// Funtions 
-
-const init = function (value) {
-  userInput.value = value;
-};
-
-const reuse = function (event) {
-  if (!event.classList.contains("icons")) {
-    if (delNum.textContent === event.textContent) {
-      event.textContent = "AC";
-    } else delNum.textContent = "C";
-  }
-  init(value);
-};
-
-// Inserting a calculation
-
-// Event handelers 
-
-numberContainer.addEventListener("click", function (e) {
-  e.preventDefault();
-  const event = e.target;
-
-
-  if (event.classList.contains("buttons")) {
-    if (!event) return;
-    reuse(event);
-    
-    // display numbers
-    value = userInput.value += event.value;
+  constructor() {
+    userInput.value = 0;
+    // Event handelers
+    numberContainer.addEventListener("click", this.#calcCulation.bind(this));
   }
 
-  // operation with user value
+  #renderValue(value) {
+    userInput.value = value;
+  }
 
-  if (event.closest(".backspace")) {
-    // removing last charecters
-
-    value = value.slice(0, -1);
-    init(value);
-    if (value.length < 1) {
-      userInput.value = 0;
-      delNum.textContent = "AC";
+  #reuse(event) {
+    if (!event.classList.contains("icons")) {
+      if (btnDelAll.textContent === event.textContent) {
+        event.textContent = "AC";
+      }
+      btnDelAll.textContent = "C";
     }
-  }
-  //  removeing all numbers
-
-  if (event.classList.contains("del__num")) {
-    init(0);
-    value = "";
-    delNum.textContent = "AC";
+    this.#renderValue(this.#value);
   }
 
-  if (event.classList.contains("btn__equal") && value)
-    value = userInput.value = `${eval(value)}`;
-});
+  // Inserting a calculation
+
+  #calcCulation(e) {
+    e.preventDefault();
+    const event = e.target;
+
+    if (event.classList.contains("buttons")) {
+      if (!event) return;
+      this.#reuse(event);
+
+      // display numbers
+      this.#value = userInput.value += event.value;
+    }
+
+    // operation with user value
+
+    if (event.closest(".backspace")) {
+      // removing last charecters
+
+      this.#value = this.#value.slice(0, -1);
+      this.#renderValue(this.#value);
+
+      if (this.#value.length < 1) userInput.value = 0;
+      btnDelAll.textContent = "AC";
+    }
+
+    
+    //  removeing all numbers
+
+    if (event.classList.contains("btn-delete__all")) {
+      this.#renderValue(0);
+      this.#value = "";
+      btnDelAll.textContent = "AC";
+    }
+
+    if (event.classList.contains("btn__equal") && this.#value)
+      this.#value = userInput.value = `${eval(this.#value)}`;
+  }
+}
+
+
+
+
+const app = new App();
